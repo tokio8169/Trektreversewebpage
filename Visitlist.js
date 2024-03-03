4fa592b50b2d0f9235b04246e28423e366aa175b
Pankaj kori <93833439+tokio8169@users.noreply.github.com>
2024-03-04 00:33:00 +0530

    Add files via upload

---
+++ Visitlist.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const visitlist=[
+
+
+    {
+
+        id:1,
+        key:1,
+        head:"Your AI-Powered Trip On TrekTraverse",
+        parahead:"The most optimal way of travel",
+        parasub:"Craft your perfect itinerary with Trip Planner AI. Our advanced algorithms take into account your selected explore-sights, dining, and lodging preferences to create the optimal travel plan tailored just for you",
+       imgsrc:"https://tripplanner.ai/_next/image?url=%2Fillustrations%2Fdestinations2.webp&w=256&q=75",
+        
+    },
+    {
+
+        id:2,
+        key:2,
+        parahead:"Get Inspired by Others' Journeys",
+        parasub:"Extract valuable travel insights from Instagram reels and TikToks, explore the mentioned explore-sights, and effortlessly include them in your own adventure with Trip Planner AI",
+        imgsrc:"https://tripplanner.ai/_next/image?url=%2Fillustrations%2Fmap.webp&w=256&q=75"
+    },
+   
+
+
+
+    // {
+
+    //     id:3,
+    //     key:3,
+    //     name:"Ashi Ghat",
+    //     img:"https://images.unsplash.com/photo-1706186839147-0d708602587b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fEthc2hpJTIwdGVtcGxlfGVufDB8fDB8fHww",
+    //     para:"Khasi Ghat is the histotical place and having incient temples ",
+    //     price:"3500$",
+    //     visit:"Visit here"
+
+    // },
+
+    // {
+    //     id:4,
+    //     key:4,
+    //     name:"Manikarnika Ghat ",
+    //     img:"https://images.unsplash.com/photo-1706186839147-0d708602587b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fEthc2hpJTIwdGVtcGxlfGVufDB8fDB8fHww",
+    //     para:"Khasi Ghat is the histotical place and having incient temples ",
+    //     price:"4500$",
+    //     visit:"Visit here"
+    // },
+
+    // {
+    //     id:5,
+    //     key:5,
+    //     name:"Ganga Ghat",
+    //     img:"https://images.unsplash.com/photo-1706186839147-0d708602587b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fEthc2hpJTIwdGVtcGxlfGVufDB8fDB8fHww",
+    //     para:"Khasi Ghat is the histotical place and having incient temples ",
+    //     price:"4500$",
+    //     visit:"Visit here"
+    // }
+ 
+    
+
+
+];
+
+export default visitlist;
